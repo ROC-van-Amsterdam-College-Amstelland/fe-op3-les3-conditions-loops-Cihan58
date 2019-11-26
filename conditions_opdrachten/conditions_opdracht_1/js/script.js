ac527075874b331a5b68bb7871e101171666f372
Cihan Karacaburun <2083040@talnet.nl>
2019-11-26 13:05:51 +0100

    Opdracht 2 af

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -3,7 +3,7 @@ function check() {
     var saldo = document.getElementById("saldo").nodeValue;
 
     if (saldo<25) {
-        document.getElementById("tekst").innerHTML = "uw saldo is lager dan 25"
+        document.getElementById("tekst").innerHTML = "uw saldo is te laag"
     } else {
         document.getElementById("tekst").innerHTML.HTML = "";
     
